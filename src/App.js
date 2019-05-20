20413dd8c7c322533b7305be65824d6525071ac8
github username <github email address>
2019-05-20 10:57:04 -0700

    done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,92 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state ={
+      pizzas: [],
+      currentPizza: {}
+    }
+    this.getPizzas = this.getPizzas.bind(this)
+    this.handleEdit = this.handleEdit.bind(this)
+    this.formHandler = this.formHandler.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.isVegetarian = this.isVegetarian.bind(this)
+    this.getPizzas()
+  }
+
+  formHandler(ev) {
+    this.setState({
+      currentPizza: {
+        ...this.state.currentPizza,
+        [ev.target.name]: ev.target.value
+      }
+    },()=>console.log(this.state.currentPizza))
+  }
+
+  handleSubmit(ev) {
+
+    fetch('http://localhost:3000/pizzas/' + this.state.currentPizza.id, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        this.state.currentPizza
+      )
+    })
+    .then(res => res.json())
+    .then(editedPizza => {
+      let pizzaCopy = this.state.pizzas.map(piz => {
+        return (editedPizza.id === piz.id) ? editedPizza : piz
+      })
+      this.setState({
+        pizzas: pizzaCopy
+      })
+    })
+  }
+
+  getPizzas() {
+    fetch('http://localhost:3000/pizzas')
+    .then(res => res.json())
+    .then(pizzas => this.setState({pizzas}))
+  }
+
+  isVegetarian() {
+    if (this.state.currentPizza.vegetarian) {
+      this.setState({
+        currentPizza: {
+          ...this.state.currentPizza,
+          vegetarian: false
+        }
+      })
+    } else {
+      this.setState({
+        currentPizza: {
+          ...this.state.currentPizza,
+          vegetarian: true
+        }
+      })
+    }
+  }
+
+  handleEdit(currentPizza) {
+    this.setState({currentPizza})
+  }
+
   render() {
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm
+          currentPizza={this.state.currentPizza}
+          formHandler={this.formHandler}
+          handleSubmit={this.handleSubmit}
+          isVegetarian={this.isVegetarian}
+        />
+        <PizzaList pizzas={this.state.pizzas} handleEdit={this.handleEdit}/>
       </Fragment>
     );
   }
--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,12 +1,16 @@
 import React from "react"
 
-const Pizza = () => {
+function isVegetarian(pizza) {
+  return (pizza.vegetarian) ? "Yes" : "No"
+}
+
+const Pizza = (props) => {
   return(
     <tr>
-      <td>{"Replace Me With Pizza Topping"}</td>
-      <td>{"Replace Me With Pizza Size"}</td>
-      <td>{"Replace Me With Vegatarian"}</td>
-      <td><button type="button" className="btn btn-primary">Edit Pizza</button></td>
+      <td>{props.pizza.topping}</td>
+      <td>{props.pizza.size}</td>
+      <td>{isVegetarian(props.pizza)}</td>
+      <td><button type="button" className="btn btn-primary" onClick={() => props.handleEdit(props.pizza)}>Edit Pizza</button></td>
     </tr>
   )
 }
